ce53771367f4370c1de6a3716958856b49e1f3a3
Matthew Phillips <phillipsm2@gmail.com>
2019-07-07 11:42:55 -0400

    Also remove the template

--- src/shim.js
+++ src/shim.js
@@ -3,7 +3,7 @@ export const dev = `
 customElements.define('f-shadow', class extends HTMLElement {
   connectedCallback() {
     let p = this.parentNode, t = p.firstElementChild;
-    this.remove();
+    this.remove(); t.remove();
     p.attachShadow({ mode: 'open' }).append(t.content.cloneNode(true));
   }
 });`.trim();
